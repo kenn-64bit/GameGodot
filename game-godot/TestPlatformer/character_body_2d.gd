ca8885cef9eda2f33201f72e3eb038f3ce504335
Kenn Jarangue <kenn.jarangue@cvsu.edu.ph>
2026-04-23 17:54:32 +0800

    @-Test / Added Animated sprite walking L & F

--- game-godot/TestPlatformer/character_body_2d.gd
+++ game-godot/TestPlatformer/character_body_2d.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 1000.0
 const JUMP_VELOCITY = -600.0
 
 # Bobbing Settings
-@export var bob_freq = 10.0  # How fast the bob happens
-@export var bob_amp = 4.0    # How high/low the camera moves
+@export var bob_freq = 10.0
+@export var bob_amp = 5.0
 var time = 0.0
 
 @onready var camera = $Camera2D
+@onready var sprite = $AnimatedSprite2D # Make sure your AnimatedSprite2D is named exactly this
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity
@@ -21,22 +22,38 @@ func _physics_process(delta: float) -> void:
 
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
+	# Handle Movement and Directional Flipping
 	if direction:
 		velocity.x = direction * SPEED
+		# Flip the sprite based on direction
+		if direction > 0:
+			sprite.flip_h = false # Facing Right
+		else:
+			sprite.flip_h = true  # Facing Left
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	
+	# Update Animations
+	update_animations(direction)
+	
 	# Apply Bobbing Logic
 	handle_camera_bob(delta)
 
+func update_animations(direction: float) -> void:
+	if not is_on_floor():
+		# Optional: If you add a "Jump" animation later, play it here
+		pass 
+	elif direction != 0:
+		sprite.play("Run")
+	else:
+		sprite.play("Idle")
+
 func handle_camera_bob(delta: float) -> void:
-	# Only bob if we are on the floor and moving
 	if is_on_floor() and abs(velocity.x) > 0.1:
 		time += delta * bob_freq
 		camera.offset.y = sin(time) * bob_amp
 	else:
-		# Smoothly return camera to center when stopped or in air
 		time = 0
 		camera.offset.y = move_toward(camera.offset.y, 0, bob_amp * delta * 2)
