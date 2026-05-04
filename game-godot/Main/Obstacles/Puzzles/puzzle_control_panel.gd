3254c890f118323fb95de4fcbd49b89313e7bcf4
TakatoEGLM <lightningmaverickmc@gmail.com>
2026-05-04 12:06:09 +0800

    @-Game \ Added basic door - control function (no kill system)

---
+++ game-godot/Main/Obstacles/Puzzles/puzzle_control_panel.gd
@@ -0,0 +1,23 @@
+extends Node2D
+class_name PuzzleControlPanel
+
+signal panel_activated
+
+@onready var _touch: Area2D = $TouchArea
+@onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var _done: bool = false
+
+
+func _ready() -> void:
+	_touch.body_entered.connect(_on_body_entered)
+	_sprite.play(&"idle")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if _done:
+		return
+	if body is CharacterBody2D:
+		_done = true
+		_sprite.play(&"deactivate")
+		panel_activated.emit()
