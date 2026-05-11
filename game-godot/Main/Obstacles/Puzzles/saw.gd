0fad5dc9c21720c26c5bfae24c17a8ebb12a1328
Lawrence1405 <lawrencebontog52@gmail.com>
2026-05-11 22:20:55 +0800

    wall blades/saw

---
+++ game-godot/Main/Obstacles/Puzzles/saw.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var _hitbox: Area2D = $Hitbox
+
+
+func _ready() -> void:
+	_hitbox.body_entered.connect(_on_hitbox_body_entered)
+	_sprite.play(&"default")
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if not body.has_method(&"handle_hazard_death"):
+		return
+	body.callv(&"handle_hazard_death", [global_position])
